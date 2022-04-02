b41854db56cba2f1bcc9a8185147dded0ee5f34e
Lucas Harada <lucasyharada@gmail.com>
2022-04-02 14:48:57 -0300

    format tf files

--- infrastructure/satire_bot/versions.tf
+++ infrastructure/satire_bot/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 4.8"
     }
     heroku = {
