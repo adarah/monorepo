7a72fefb59949819376b4a7a8fcba00782dc2ee4
Lucas Harada <lucasyharada@gmail.com>
2022-04-02 23:19:45 -0300

    fix current aws region

--- infrastructure/satire_bot/main.tf
+++ infrastructure/satire_bot/main.tf
@@ -32,16 +32,18 @@ resource "aws_iam_role" "assume_role" {
   })
 }
 
+data "aws_region" "current" {}
+
 resource "aws_lambda_function" "ping_pong" {
   function_name = "ping_pong"
   role          = aws_iam_role.assume_role.arn
   runtime       = "go1.x"
-  filename =  "bazel-bin/satire-bot/ping_pong_handler.zip"
+  filename      = "bazel-bin/satire-bot/ping_pong_handler.zip"
   environment {
     variables = {
       SATBOT_PING_PONG_ARN = aws_lambda_function.ping_pong.arn
       SATBOT_DISCORD_TOKEN = ""
-      SATBOT_AWS_REGION    = aws.region
+      SATBOT_AWS_REGION    = data.aws_region.current.name
     }
   }
 }
